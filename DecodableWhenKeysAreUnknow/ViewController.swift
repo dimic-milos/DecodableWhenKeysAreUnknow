ad7e8e98f4fac5d329a6ea473ed374b8a375d8be
Milos Dimic <dimic.milos@icloud.com>
2019-05-05 10:35:49 +0200

    initial implementation ;

--- DecodableWhenKeysAreUnknow/ViewController.swift
+++ DecodableWhenKeysAreUnknow/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let json = """
+    {
+        "BTC_BCN":{
+            "last":"0.00000057",
+            "percentChange":"0.03636363",
+            "baseVolume":"47.08463318"
+        },
+        "BTC_BELA":{
+            "last":"0.00001281",
+            "percentChange":"0.07376362",
+            "baseVolume":"5.46595029"
+        }
+    }
+    """.data(using: .utf8)!
+    
+    struct Pair {
+        let name: String
+        let details: Details
+        
+        struct Details: Codable {
+            let last, percentChange, baseVolume: String
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        decode()
+    }
+    
+    func decode() {
+        if let pairsDictionary = try? JSONDecoder().decode([String: Pair.Details].self, from: json) {
+            
+            var pairs: [Pair] = []
+            for (name, details) in pairsDictionary {
+                let pair = Pair(name: name, details: details)
+                pairs.append(pair)
+            }
+            
+            print(pairs)
+        }
     }
 
 
